1a415daa997e0cd24eabcc5717cad66942a0963e
Maxim <furymaxim@gmail.com>
2019-04-12 02:23:40 +0300

    splashScreen + ToolBar

---
+++ app/src/main/java/com/example/rsreu_app/SplashActivity.java
@@ -0,0 +1,59 @@
+package com.example.rsreu_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static java.lang.Thread.sleep;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView appImage;
+        TextView appName;
+
+        appImage = findViewById(R.id.imageView);
+        appName = findViewById(R.id.appName);
+
+        /*
+        * Загружаем анимацию из xml-файла и запускаем
+        */
+        Animation animationTransition = AnimationUtils.loadAnimation(this,R.anim.transition);
+        appImage.startAnimation(animationTransition);
+        appName.startAnimation(animationTransition);
+
+        /*
+         * Создаем переменную класса Intent, планируется реализация сверки расписания на сервере и в БД,
+         * при наличии Интернета на устройстве, если изменилось - то меняем на актуальное, и выводим уведомление пользователю
+         */
+        Intent intent = new Intent(this,MainActivity.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                   sleep(5000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+
+        timer.start();
+
+
+    }
+}
