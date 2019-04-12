5389b3b233079f075a9861850764e90c0c989cce
Maxim <furymaxim@gmail.com>
2019-04-12 20:29:10 +0300

    splashScreen + ToolBar

--- app/src/main/java/com/example/rsreu_app/MainActivity.java
+++ app/src/main/java/com/example/rsreu_app/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.rsreu_app;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ScheduleFragment()).commit();
+        }
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_schedule);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_schedule:
+                    selectedFragment = new ScheduleFragment();
+                    break;
+                case R.id.nav_news:
+                    selectedFragment = new NewsFragment();
+                    break;
+                case R.id.nav_settings:
+                    selectedFragment = new SettingsFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
