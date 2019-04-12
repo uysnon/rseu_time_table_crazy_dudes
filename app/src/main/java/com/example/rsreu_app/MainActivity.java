ee263c68537244435a9162571ce124bd3ad44cab
Maxim <furymaxim@gmail.com>
2019-04-12 20:50:11 +0300

    Commit №2. BottomNavigationView + SharedPreferences(group number, just started)

--- app/src/main/java/com/example/rsreu_app/MainActivity.java
+++ app/src/main/java/com/example/rsreu_app/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.rsreu_app;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    EditText groupNumber;
+    public static final String myPreference = "myPref";
+    public static final String groupKey = "groupKey";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        groupNumber = findViewById(R.id.groupNumber);
+
+        sharedPreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE);
+        
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
