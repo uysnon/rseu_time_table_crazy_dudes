31a160de794a0419707e88aa26a4aa587efd4952
uysnon <avgorkin1@gmail.com>
2019-05-26 22:15:52 +0300

    последняя версия

--- app/src/main/java/com/example/rsreu_app/MapsActivity.java
+++ app/src/main/java/com/example/rsreu_app/MapsActivity.java
@@ -1,7 +1,7 @@
 package com.example.rsreu_app;
 
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
